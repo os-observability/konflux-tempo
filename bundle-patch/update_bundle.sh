b9ca58b281f4610d95c695dc12395f9ad6ff07bf
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-11-12 16:15:58 +0000

    Update tempo-tempo to 72ac3ed
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -3,7 +3,7 @@
 set -e
 
 # The pullspec should be image index, check if all architectures are there with: skopeo inspect --raw docker://$IMG | jq
-export TEMPO_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo@sha256:fecea08f538affd375204e4dcc192ccfebb707f855c60ce3af5f057d79225643"
+export TEMPO_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo@sha256:72ac3ed2c813b66e8c4e8e2d411ad51a233a8a62d7e1d7773c7c4a079d9167f3"
 # Separate due to merge conflicts
 export TEMPO_QUERY_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo-query@sha256:1f5a23c0a2660f779925644c181859b94f12101f08268fcd0a085c1760246f1f"
 # separate due to merge conflicts
