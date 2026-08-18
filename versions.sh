806881657135ccdb45eedead1564045bd62400ad
Andreas Gerstmayr <agerstmayr@redhat.com>
2026-08-18 17:20:46 +0200

    Update tempo-operator to v0.22.0 for 3.11.0 (#909)
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- versions.sh
+++ versions.sh
@@ -11,14 +11,18 @@ RHOSDT_VERSION=3.11
 MIN_OPENSHIFT_VERSION=4.12
 
 
-echo "Fetching tags of all submodules..."
+echo "Cloning submodules..."
+git submodule update --recursive --remote
 git -C tempo remote add upstream https://github.com/grafana/tempo.git 2>/dev/null || true
 git -C tempo-operator remote add upstream https://github.com/grafana/tempo-operator.git 2>/dev/null || true
 git -C api remote add upstream https://github.com/observatorium/api.git 2>/dev/null || true
 git -C opa-openshift remote add upstream https://github.com/observatorium/opa-openshift.git 2>/dev/null || true
 git -C jaeger remote add upstream https://github.com/jaegertracing/jaeger.git 2>/dev/null || true
 git -C jaeger-ui remote add upstream https://github.com/jaegertracing/jaeger-ui.git 2>/dev/null || true
+
+echo "Fetching tags of all submodules..."
 git submodule foreach --recursive "git fetch --tags upstream"
+
 OPERATOR_VERSION=$(cd tempo-operator && git describe --tags --abbrev=0 | sed 's/^v//')
 OPERATOR_TEMPO_VERSION=$(grep -oP '^TEMPO_VERSION \?= \K.*' tempo-operator/Makefile)
 TEMPO_VERSION=$(cd tempo && git describe --tags --abbrev=0 | sed 's/^v//')
